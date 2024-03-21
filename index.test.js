391acbdd522dad1f591cf57604bce593bfbcb4a8
Olena Reukova <kronverk01@gmail.com>
2024-03-21 12:36:01 +0000

    solution

--- index.test.js
+++ index.test.js
@@ -1,8 +1,19 @@
 const { getQuote } = require('./index');
+const { fetchData } = require('./fetch-data');
+
+jest.mock('./fetch-data'); // this line sayse: we are going to put mock data from file fetch-data,
+// in this example I  used mock data like name:Henry, content: Hello world
 
 describe('getQuote', () => {
   it('returns a quote', async () => {
+    fetchData.mockResolvedValue({
+      id: 1440194,
+      originator: {
+        name: 'Henry',
+      },
+      content: 'Hello friend,',
+    });
     const quote = await getQuote();
-    expect(typeof quote).toEqual('string');
+    expect(quote).toEqual('Hello friend, (Henry)');
   });
 });
